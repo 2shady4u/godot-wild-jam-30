23f85084bde96c7ca67e469bb0c7d365d7fa3f93
2shady4u <jd16581556@gmail.com>
2021-02-13 02:32:17 +0100

    Add basic room

--- src/game/Player.gd
+++ src/game/Player.gd
@@ -2,3 +2,19 @@ extends KinematicBody2D
 
 func _ready():
 	pass # Replace with function body.
+
+func _physics_process(delta):
+	var move_direction := Vector2.ZERO
+	var move_speed := 200
+
+	if Input.is_action_pressed("move_down"):
+		move_direction.y += 1
+	if Input.is_action_pressed("move_up"):
+		move_direction.y -= 1
+	if Input.is_action_pressed("move_left"):
+		move_direction.x -= 1
+	if Input.is_action_pressed("move_right"):
+		move_direction.x += 1
+
+	var normalized_direction := move_direction.normalized()
+	var _linear_velocity := move_and_slide(normalized_direction*move_speed)
